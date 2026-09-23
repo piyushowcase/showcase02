878a28e6716628fdaff7e6cc71537a854889b9f2
piyushowcase <npiyush747@gmail.com>
2026-09-23 12:30:30 +0530

    Update cookie security settings in authController

--- 1.websiteBuilder/server/controller/authController.js
+++ 1.websiteBuilder/server/controller/authController.js
@@ -19,8 +19,8 @@ export const googleauth = async (req, res) => {
             const token= await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
             res.cookie("token",token,{
                 httpOnly:true,
-                secure:false,
-                sameSite:"strict",
+                secure:true,
+                sameSite:"none",
                 maxAge:7*24*60*60*1000
             })
                 
@@ -37,8 +37,8 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
         })
 
         return res.status(200).json({ success: true, message: "Logged out successfully" })
