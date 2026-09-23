aaf0baf30a9f66b4a492420b451579ecfdd5fb67
piyushowcase <npiyush747@gmail.com>
2026-09-23 14:09:12 +0530

    Update CORS origin for server configuration

--- 1.websiteBuilder/server/index.js
+++ 1.websiteBuilder/server/index.js
@@ -16,7 +16,7 @@ stripeWebHook)
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://showcase02-1.onrender.com',
+    origin: 'https://showcase02-2.onrender.com/',
     credentials: true,
 
 }))
